71c0a7619a6458181f11219d529efd5e94f93d9d
ianooh <iankimeli22@gmail.com>
2024-09-13 09:11:47 +0300

    Week 2 assignment

---
+++ List.py
@@ -0,0 +1,11 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1,15)
+my_list.extend([50, 60, 70])
+del my_list[7]
+my_list.sort()
+index_of_30 = my_list.index(30)
+print(index_of_30)
